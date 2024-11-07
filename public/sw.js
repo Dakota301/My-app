b1c302cccc6c2ecd8c4df5d9c7c965894f7a7c02
dakota <57602032+Dakota301@users.noreply.github.com>
2024-11-07 00:42:07 -0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+// public/sw.js
+const CACHE_NAME = "meu-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
+];
+
+// Instalação do service worker e armazenamento no cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Responder com arquivos do cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
